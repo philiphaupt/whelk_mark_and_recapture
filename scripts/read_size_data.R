f07159abec7ff6b6f6086a2b9c4da82538c00a26
philiphaupt <philip.haupt@gmail.com>
2023-09-25 20:22:31 +0100

    minor edits change reading file instructions to direct it to specific directory

--- scripts/read_size_data.R
+++ scripts/read_size_data.R
@@ -3,5 +3,7 @@
 library("tidyverse")
 
 
-# REad in Excel table data
-readxl::read_xlsx("./data/")
+# Read in Excel table data
+size_dat <- readxl::read_xlsx("C:/Users/Phillip Haupt/Documents/SUSTAINABLE FISHERIES/whelks/whelk__mark_and_recapture_2023/size_measurements/whelk_photo_measurement_data_margate_hook_june-august_2023_compiled_20230824.xlsx") 
+
+                              
