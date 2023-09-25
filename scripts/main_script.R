011ca81a6e1cfa9cc1dd7b73976c2ee2c2f7db86
philiphaupt <philip.haupt@gmail.com>
2023-09-25 19:17:10 +0100

    first upload to get started

---
+++ scripts/main_script.R
@@ -0,0 +1,20 @@
+# Main Script: Whelk Mark and recapture analysis
+# Author: Philip Haupt
+# Date: 25/09/2023
+#----------------------
+# Three components:
+#. 1 Mark and recapture
+#. 2 Size measurements from marked and recaptured whelks
+#. 3 Water temperature data analysis - in situ water logger measurements
+#----------------------
+
+# Aims: Under what is happening with the whelk stocks off the North Kent Coast after the collapse in catches and reports of morbind and dead whelks in pots 2022 (Aug - Dec)
+# Objectives: Monitor whelk population trends at near and far affected sites over time
+# Understand water temperature trends at sea bed at near and far affected site and the correlation between whelk popualtion/catches and water temperature
+
+#---------------------
+# Size measurements
+
+## Read in size data (from photogrammetric analysis)
+file.edit("./scripts/read_size_data.R")
+
---
+++ scripts/read_size_data.R
@@ -0,0 +1,7 @@
+# aim read in size data
+
+library("tidyverse")
+
+
+# REad in Excel table data
+readxl::read_xlsx("./data/")
