1a6b59347e0b54a6498ae132b2aa36b2e4911d5c
philiphaupt <philip.haupt@gmail.com>
2023-10-31 09:35:02 +0000

    upload jolly seber description

---
+++ scripts/jolly-seber_pop_estimate.R
@@ -0,0 +1,14 @@
+# The calculation of the total population is specified as follows:
+
+# m2 – number of marked individuals recaptured on the second (subsequent) sampling event. 
+# n2 – total number caught on second sampling event. 
+# n1 - total number caught and marked on the first marking event.
+# N – total population.
+# The total population is then estimated as:  N ̂=(n_1+1)(n_2+ 1)/(m_2+ 1) - 1 
+# 
+# Such that m_2/n_2 =n_1/N  where N is the total (estimated) population.
+# This can be expressed as  N=(〖(n〗_1*n_2))/m_2   
+
+data_prep
+
+
