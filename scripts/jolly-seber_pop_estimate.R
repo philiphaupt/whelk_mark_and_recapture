5b79ceb873f5dc50be871df66aa3e54d47f41674
Philip Haupt <philip.haupt@kentandessex-ifca.gov.uk>
2023-10-31 10:53:02 +0000

    upload updates to Lincoln-Peterson estimates and Jolly-Seber

--- scripts/jolly-seber_pop_estimate.R
+++ scripts/jolly-seber_pop_estimate.R
@@ -1,3 +1,6 @@
+library("mra")
+# Cormack-Jolly-Seber
+
 # The calculation of the total population is specified as follows:
 
 # m2 – number of marked individuals recaptured on the second (subsequent) sampling event. 
@@ -9,6 +12,30 @@
 # Such that m_2/n_2 =n_1/N  where N is the total (estimated) population.
 # This can be expressed as  N=(〖(n〗_1*n_2))/m_2   
 
-data_prep
+
+
+
+
+
+# examples from existing packages:
+# data(dipper.data)
+# data(dipper.histories)
+# 
+# #install.packages("marked") # first time only
+# library(marked)
+# 
+# # Load dipper data (included in "marked")
+# data(dipper, package = "marked")
+# 
+# # Examine data structure
+# head(dipper)
+# 
+# # First, process data (Notice model = "JS", previous version = "CJS")
+# dipper.js.proc <- process.data(dipper, 
+#                                model = "JS", 
+#                                groups = "sex")
+# # Second, make design data (from processed data)
+# dipper.js.ddl <- make.design.data(dipper.js.proc)
+
 
 
--- scripts/lincoln-peterson_pop_estimate.R
+++ scripts/lincoln-peterson_pop_estimate.R
@@ -8,17 +8,17 @@ library("mra")
 # Formula: N = (C/R)*M
 
 # M - Individuals marked (M1)  = total number caught first event/
-# C - Total number captured event 2/3
-# R - Total number recaptured at event 2/3.
+# C - Total number captured event 2/3 # I am using 3
+# R - Total number recaptured at event 2/3. # I am using 3
 
 my_lincoln_peterson_index <- function(C, R, M){
   N = (C/R)*M
 }
 
-
+# data prep----------------
 dat_prep <- mr_dat %>%
-  mutate(string_section = str(fkey_opcode, ))
-  group_by(location_abbrv, date, string_number, deploy_type) %>%
+  mutate(string_section = str_sub(fkey_opcode, 2,2)) %>% 
+  group_by(location_abbrv, date, string_number, string_section, deploy_type) %>%
   summarise(
     r1_totals = sum(recapture_1),
     r2_totals = sum(recapture_2),
@@ -38,20 +38,51 @@ dat_prep <- mr_dat %>%
       sum(total_unbanded_whelks + r2_totals),
       NA
     ),
-    #sum(total_unbanded_whelks + r2_totals),
-    # sum(total_unbanded_whelks + r1_totals + r2_totals)
     proportion_marked_recaptures_m1r1 = r1_totals / total_whelks_caught_m1r1,
     proportion_marked_recaptures_m1r2 = r1_totals / total_whelks_caught_m1r2,
     proportion_marked_recaptures_m2r2 = r2_totals / total_whelks_caught_m2r2
   )
 
-lincoln_peterson_data <- proportion_recaptures %>% 
-  group_by(fkey_opcode) %>% 
-  mutate(C = )
-  
-  
+# Margate data prep-------
+#M
+margate_marked1 <- dat_prep %>% 
+  ungroup() %>% 
+  dplyr::filter(location_abbrv == "Margate") %>% 
+  dplyr::arrange(date) %>% 
+  dplyr::slice(1:4) %>% 
+  group_by(string_number, string_section, deploy_type) %>% 
+  summarise(M = sum(total_unbanded_whelks))
 
+#R
+margate_marked1_recaptured2 <- dat_prep %>% 
+  ungroup() %>% 
+  dplyr::filter(location_abbrv == "Margate") %>% 
+  dplyr::arrange(date) %>% 
+  dplyr::slice(7:8) %>% 
+  group_by(string_number, string_section, deploy_type) %>% 
+  summarise(R = sum(r1_totals))
 
+#C
+margate_captured2 <- dat_prep %>% 
+  ungroup() %>% 
+  dplyr::filter(location_abbrv == "Margate") %>% 
+  dplyr::arrange(date) %>% 
+  dplyr::slice(7:8) %>% 
+  group_by(string_number, string_section, deploy_type) %>% 
+  summarise(C = sum(total_whelks_caught_m1r2))
+
+# Lincoln_Peterson data final
+margate_lp_dat <- margate_marked1 %>% bind_cols(margate_marked1_recaptured2) %>% bind_cols(margate_captured2) %>% 
+  select(string_number = string_number...1,
+         string_section = string_section...2,
+         M,
+         C,
+         R)
+
+#calculation---------
+margate_lp_pop <- my_lincoln_peterson_index(C = margate_lp_dat$C, R = margate_lp_dat$R, M = margate_lp_dat$M)
+
+# Repeat this for Whitstable when more data
 
 
   
