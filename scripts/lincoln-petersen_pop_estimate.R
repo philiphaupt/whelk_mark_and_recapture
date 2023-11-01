2456acd986ef053f14a55c1c617ab27d29be26b6
Philip Haupt <philip.haupt@kentandessex-ifca.gov.uk>
2023-11-01 12:02:23 +0000

    rename Peterson to Petersen

--- scripts/lincoln-petersen_pop_estimate.R
+++ scripts/lincoln-petersen_pop_estimate.R
@@ -4,20 +4,20 @@ library(data.table)
 ## Construct data in required input format for ... analysis
 ## planning on using Jolly-Seber analysis if possible because Suspect the population to be open.
 
-# Lincoln - Peterson index
+# Lincoln - Petersen index
 ## NOTES: 
 # Only m1-r2 - population appears fully mixed between pots suggesting to only use this data - but this may be influenced by micro scale food availability/movement dynamics?
 # using the first two days as marking - and then only counting individuals that were recaptured on the third event, because a the time of the second event the population was not fully mixed
 
-# Lincoln - Peterson index
+# Lincoln - Petersen index
 # Formula: N = (C/R)*M
 
 # M - Individuals marked (M1)  = total number caught first event. (My case first two events from Margate)
 # C - Total number captured during event 3 # I am using 3, COULD ALSO HAVE USED EVENT 2, BUT DID NOT SATISFY ASSUMPTION THAT THE POPULATION HAS FULLY MIXED
 # R - Total number recaptured at event 3. # I am using 3
 
-# Lincoln-Peterson as a function:
-my_lincoln_peterson_index <- function(C, R, M){
+# Lincoln-Petersen as a function:
+my_lincoln_Petersen_index <- function(C, R, M){
   N = (C/R)*M
 }
 
@@ -52,7 +52,7 @@ dat_prep <- mr_dat %>%
     proportion_marked_recaptures_m2r2 = r2_totals / total_whelks_caught_m2r2
   )
 
-# Lincoln-Peterson data prep-------
+# Lincoln-Petersen data prep-------
 # M
 marked_1 <- dat_prep %>% 
   ungroup() %>% 
@@ -102,7 +102,7 @@ captured <- dat_prep %>%
             ) %>% 
   ungroup()
 
-# Lincoln_Peterson data final
+# Lincoln_Petersen data final
 lp_dat <- marked_n %>%
   bind_cols(recaptured %>% select(Rm1, Rm2)) %>%
   bind_cols(captured %>% select(C)) %>%
@@ -130,17 +130,17 @@ lp_dat <- marked_n %>%
 
 #calculation---------
 # Per string
-lp_pop_vector <- my_lincoln_peterson_index(C = lp_dat$C, R = lp_dat$Rm1, M = marked_1$M1) %>% round(.,0) %>% as.vector()
+lp_pop_vector <- my_lincoln_Petersen_index(C = lp_dat$C, R = lp_dat$Rm1, M = marked_1$M1) %>% round(.,0) %>% as.vector()
 lp_pop_matrix <- matrix(lp_pop_vector, nrow = 3, ncol = 2, byrow = TRUE) 
 print(lp_pop_matrix)
 
 
 
-lp_pop_matrix %>% 
+lp_pop_df <- lp_pop_matrix %>% 
   as_tibble() %>% 
-  rename(N_a = V1, 
+  dplyr::rename(N_a = V1, 
          N_b = V2)
-print(lp_pop_matrix)
+rm(lp_pop_matrix, lp_pop_vector)
 
 
 # Repeat this for Whitstable when more data
