495b9d3038e101f014b048a45e1cfa5b80c586fd
Philip Haupt <philip.haupt@kentandessex-ifca.gov.uk>
2023-11-14 12:09:50 +0000

    add in multiple plot functionality

--- scripts/schematic_plot_of_whelk_densities.R
+++ scripts/schematic_plot_of_whelk_densities.R
@@ -4,6 +4,7 @@
 library(ggplot2)
 library(ggimage)
 library(png)
+library(cowplot)
 
 # Set a seed for reproducibility
 set.seed(123)
@@ -45,21 +46,24 @@ generate_circular_plot <- function(n_points, color_ratio, max_lim, min_lim, samp
   data <- data.frame(
     X = runif(n = n_points, min = min_lim, max = max_lim),
     Y = runif(n = n_points, min = min_lim, max = max_lim),
-    image = sample(c("./images/whelk_lr.png"), size = n_points, replace = TRUE),
+    image = sample(c("./images/whelk_lr_no_bg.png"), size = n_points, replace = TRUE),
     color = sample(colors, size = n_points, replace = TRUE)
   ) %>% 
     mutate(alphas = if_else(color == "white", 0.01, 0.3)) # adds dynamically determined by condition alpha value
   
   ggplot(data, aes(x = X, y = Y)) +
-    geom_image(aes(image = image), size = 0.05) +
+    geom_image(aes(image = image), size = 0.075) +
     geom_point(aes(colour = color, fill = color, alpha = alphas), size = 5, shape = 21) +
     scale_color_identity() +
     scale_fill_identity() +
-    scale_alpha_identity()+
+    scale_alpha_identity() +
     coord_polar() +
     theme_minimal() +
-    theme(legend.position = "none")+
-    labs(x = "", y = "", title = paste0("Whelks in ", sample_section,", Pot number: ", pot_number))
+    theme(legend.position = "none") +
+    labs(x = NULL, y = NULL, title = paste0("Sample: ", sample_section, ", Pot number: ", pot_number)) +
+    theme(panel.background = element_rect(fill = "white"),
+          plot.background = element_rect(fill = "white"),
+          axis.text = element_blank())
 }
 
 
@@ -67,20 +71,57 @@ generate_circular_plot <- function(n_points, color_ratio, max_lim, min_lim, samp
 #generate_circular_plot(n_points = 62, color_ratio = c(57, 4, 1), max_lim = 100, min_lim = 1, sample_section = paste0("Sample Section ", i), pot_number = j)
 #----------
 
+# # Set the seed for reproducibility
+# set.seed(123)
+# 
+# # Number of strings
+# num_sampling_sections <- 2
+# 
+# 
+# for (i in 1:num_sampling_sections) {
+#   
+#   # Number of iterations (pots)
+#   num_pots <- 5
+#   
+#   # Create and display plots for each iteration
+#   for (j in 1:num_pots) {
+#     pot_number = j
+#     n_points <- 62
+#     color_ratio <-
+#       sample(
+#         x = list(c(57, 5, 0), c(57, 4, 1), c(57, 3, 2), c(57, 2, 3), c(57, 1, 4)),
+#         size = 1,
+#         prob = c(0.1, 0.6, 0.1, 0.07, 0.03)
+#       ) %>% unlist()
+#     max_lim <- 100
+#     min_lim <- 1  # sample(1:100, 1)
+#     
+#     print(generate_circular_plot(n_points, color_ratio, max_lim, min_lim, sample_section = i, pot_number = j))
+#     
+#   }
+# }
+
+#--
+# Install and load necessary packages
+#install.packages("cowplot")
+
+
 # Set the seed for reproducibility
 set.seed(123)
 
-# Number of strings
+# Number of sampling sections
 num_sampling_sections <- 2
 
+# Create a list to store plots
+plot_list <- list()
 
 for (i in 1:num_sampling_sections) {
-  sample <- paste0("Sample Section ", i)
+  sample_section <- i
   
   # Number of iterations (pots)
   num_pots <- 5
   
-  # Create and display plots for each iteration
+  # Create and store plots for each iteration
   for (j in 1:num_pots) {
     pot_number = j
     n_points <- 62
@@ -93,8 +134,14 @@ for (i in 1:num_sampling_sections) {
     max_lim <- 100
     min_lim <- 1  # sample(1:100, 1)
     
-    print(generate_circular_plot(n_points, color_ratio, max_lim, min_lim, sample_section = sample, pot_number = j))
-    
+    plot_list[[length(plot_list) + 1]] <- generate_circular_plot(n_points, color_ratio, max_lim, min_lim, sample_section = sample_section, pot_number = j)
   }
 }
+
+# Arrange plots in a grid
+grid_plot <- plot_grid(plotlist = plot_list, ncol = 5)
+print(grid_plot)
+# Save the grid plot
+ggsave("./outputs/grid_plot.png", grid_plot, width = 15, height = 8, units = "in")
+
 
