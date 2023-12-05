86a7a8b4c20a068c8c1d18517d8abfda4bc03ddb
Philip Haupt <philip.haupt@kentandessex-ifca.gov.uk>
2023-12-05 14:39:29 +0000

    tidy up housekeeping

--- scripts/read_TD_data.R
+++ scripts/read_TD_data.R
@@ -1,7 +1,7 @@
 # Read Temperature Depth log data
 library(tidyverse)
-#library(httr)
 library(readxl)
+library(data.table)
 
 #NOT RUN: This solution will not work if you are using OneDrive for Business and your admin has disabled the use of public file sharing via links
 # url1<-"https://keifca-my.sharepoint.com/personal/philip_haupt_kentandessex-ifca_gov_uk/Documents/whelk_pot_data_logger_water_temperature_20230502-20230809.xlsx?web=1"
@@ -31,16 +31,39 @@ td_dat_early_compiled <- readxl::read_xlsx(paste0(my_wd, "/data/whelk_pot_data_l
                          "emerged", 
                          "submerged"),
          temp_change = abs(temperature - lag(temperature, default = first(temperature))),
-         temp_change_status = ifelse(temp_change > 1, "remove", "keep")
+         temp_change_status = ifelse(temp_change > 0.35, "remove", "keep")
 ) %>% 
-  filter(status == "submerged" & temp_change_status == "keep") %>% 
+  mutate(start_date_status == case_when(location %like% "Margate" & dates > "2023-06-12" ~ "keep", "remove")) %>%  # start date filter
+  filter(status == "submerged" & temp_change_status == "keep" & start_date_status == "keep") %>%   
   ungroup()
 
+##---------
+td_dat_early_compiled <- readxl::read_xlsx(paste0(my_wd, "/data/whelk_pot_data_logger_water_temperature_20230502-20230809_copy_20231011.xlsx")) %>% 
+  select(pkey_td_log_id,
+         fkey_record_on_deployment,
+         dates = date_correct_all...11,
+         times = time,
+         date_time = date_time,
+         temperature = temperature_degrees_celsius,
+         depth = water_depth_meter,
+         status = `...16`,
+         logger_id = Logger_id,
+         location) %>% 
+  mutate(dates = format(as.POSIXct(date_time), "%Y-%m-%d"),
+         times = format(as.POSIXct(date_time), "%H:%M")) %>% 
+  group_by(dates) %>% 
+  mutate(status = ifelse(abs(depth - lag(depth, default = first(depth))) > 0.5| abs(depth - lag(depth, default = first(depth))) < -0.5, 
+                         "emerged", 
+                         "submerged"),
+         temp_change = abs(temperature - lag(temperature, default = first(temperature))),
+         temp_change_status = ifelse(temp_change > 0.35, "remove", "keep"),
+         start_date_status = case_when(location %like% "Margate" & dates > "2023-06-12" ~ "keep",
+                                       location %like% "Studhill" ~ "keep",
+                                       TRUE ~ "remove")) %>%  # start date filter
+  filter(status == "submerged" & temp_change_status == "keep" & start_date_status == "keep") %>%   
+  ungroup()
 
-
-
-
-
+# Read in second file - will need to make this a function in future
 td_add_on <- read_table("./data/20231002 - 20231106_studhill_data.txt", skip = 14, 
                         col_names = FALSE
 ) %>% 
@@ -51,13 +74,14 @@ td_add_on <- read_table("./data/20231002 - 20231106_studhill_data.txt", skip = 1
          depth = X5) %>% 
   mutate(dates = dmy(dates),  # Parse the date using lubridate
          dates = format(dates, "%Y-%m-%d"),  # Reformat the date
-         times = format(as.POSIXct(times), "%H:%M"),
+         times = format(as.POSIXct(times, format = "%H:%M"), "%H:%M:%S"),
+         date_time = as.POSIXct(paste(dates, times), format = "%Y-%m-%d %H:%M:%S"),
          status = ifelse(abs(depth - lag(depth, default = first(depth))) > 0.5 | 
                            abs(depth - lag(depth, default = first(depth))) < -0.5, 
                          "emerged", 
                          "submerged"),
          temp_change = abs(temperature - lag(temperature, default = first(temperature))),
-         temp_change_status = ifelse(temp_change > 1, "remove", "keep")
+         temp_change_status = ifelse(temp_change > 0.35, "remove", "keep")
   ) %>% 
   filter(status == "submerged" & temp_change_status == "keep")
 
@@ -74,10 +98,6 @@ logger_id <- read_table("./data/20231002 - 20231106_studhill_data.txt", n_max = 
   slice(13) %>%
   pull(X3) %>% 
   str_extract("C.*(?=DAT)")
-
-# # Add logger_id column to td_dat
-# td_dat <- td_dat %>%
-#   mutate(logger_id = logger_id)
 
 # Add logger_id column to td_add_on
 td_add_on <- td_add_on %>%
